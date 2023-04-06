cdb98c342d82d2310a25f8e4e09537edae4ded72
RamiroLuque <raluque1987@gmail.com>
2023-04-05 21:43:37 -0300

    casiterminando

--- TrabajoFinal/src/app/app.module.ts
+++ TrabajoFinal/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     LogoAPComponent,
     SocialComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    EducacionComponent,
+    
   ],
   imports: [
     BrowserModule,
