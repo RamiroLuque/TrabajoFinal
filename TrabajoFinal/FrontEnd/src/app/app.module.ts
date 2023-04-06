df59bee4bb28ef64dffdc7041055327a74e474a2
RamiroLuque <raluque1987@gmail.com>
2023-04-06 14:23:13 -0300

    creacion de carpeta frontend
    FrontEnd funcional

--- TrabajoFinal/FrontEnd/src/app/app.module.ts
+++ TrabajoFinal/FrontEnd/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
