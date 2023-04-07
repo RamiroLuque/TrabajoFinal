39289950b9aa775a83fc5d4baf8e2e7447e697c1
RamiroLuque <raluque1987@gmail.com>
2023-04-07 11:26:26 -0300

    frontend

---
+++ TrabajoFinal/BackEnd/src/main/java/com/portfolio/RRLG/RrlgApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.RRLG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrlgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrlgApplication.class, args);
+	}
+
+}
---
+++ TrabajoFinal/BackEnd/src/test/java/com/portfolio/RRLG/RrlgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.RRLG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrlgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
